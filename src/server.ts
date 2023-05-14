442f58a2c0d29a537e1f5babcac52701fec460cc
luizfernando <luiz-fsilva37@hotmail.com>
2023-05-13 23:07:55 -0300

    Tratando env com Zod

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,10 @@
 import fastify from 'fastify'
 import { knex } from './database'
-// import crypto from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
-const port = 3333
 
 app.get('/hello', async () => {
-  // const transaction = await knex('transactions')
-  //   .insert({
-  //     id: crypto.randomUUID(),
-  //     title: 'Transação de teste',
-  //     amount: 1000,
-  //   })
-  //   .returning('*')
-  // return transaction
-
   const transactions = await knex('transactions')
     .where('amount', 500)
     .select('*')
@@ -24,8 +14,8 @@ app.get('/hello', async () => {
 
 app
   .listen({
-    port,
+    port: env.PORT,
   })
   .then(() => {
-    console.log(`Active app at: http://localhost:${port}`)
+    console.log(`Active app at: http://localhost:${env.PORT}`)
   })
